cd1290b425919b199b4129ae85c4c01d77e240a4
Anasdevs <anassaifi1108@gmail.com>
2023-08-23 19:55:13 +0530

    Completed home page

--- anas-portfolio/src/Components/Home.jsx
+++ anas-portfolio/src/Components/Home.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import './Home.css'
 import themeButton from './sun-line.png'
+import anasImage2 from './anasImage6.jpeg'
+import linkedin from './linkedin.svg'
+import github from './github.svg'
+import instagram from './instagram.svg'
+import arrowdown from './arrowdown.svg'
+
+
 
 export default function Home(){
     return(
         <div className="main">
+            <div className="upperContainer">
+
         <div className="header">
 
         <div className="myName">
         <h2>Anas</h2>
         </div>
         <div className="themeIcon">
-                <li><img src={themeButton} alt="Theme Button" /></li>
+                <img src={themeButton} alt="Theme Button" />
         </div>
         </div>
         <div className="myInfo">
@@ -23,14 +32,25 @@ export default function Home(){
             <button className="download-cv">Download CV</button>
             <button className="about-me">About Me</button>
         </div>
+            </div>
 
-        {/* <div className="navbar">
+        <div className="midContainer">
+        <div className="navbar">
             <ul>
-                <li>Home</li>
-                <li>About me</li>
-                <li>Contact</li>
+                <li><img src={linkedin} alt="" /></li>
+                <li><img src={github} alt="" /></li>
+                <li><img src={instagram} alt="" /></li>
             </ul>
-        </div> */}
+        </div>
+        <div className="myImage">
+            <img src={anasImage2} alt="" />
+        </div>
+       
+        <div className="scrollInfo">
+        <span> <p> 
+                Scroll Down <img src={arrowdown} alt="" /> </p></span>
+        </div>
+        </div>
         </div>
 
 );
